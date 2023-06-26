25100d2d967c1321bd15e55a645182ee1e544665
byoso <emailvinz@gmail.com>
2023-06-26 21:04:23 +0200

    WIP: collection exchange

--- src/views/campains/collections/CollectionsView.vue
+++ src/views/campains/collections/CollectionsView.vue
@@ -1,6 +1,12 @@
 <template>
   <div class="container">
+
+
     <h1 class="title">Collections</h1>
+
+
+    <button class="button is-primary is-small m-2" @click="collectionCreate">+ New Collection</button>
+    <button class="button is-secondary is-small m-2" @click="">Open exchange interface</button>
 
     <div @click="showMyCollections = !showMyCollections" class="topic">My Collections
       <div class="is-pulled-right mr-2">
@@ -10,7 +16,6 @@
     </div>
 
     <div v-if="showMyCollections">
-      <button class="button is-success is-small m-2" @click="collectionCreate">+ New Collection</button>
       <div v-if="myCollections.length">
         <table class="table is-fullwidth">
           <thead>
@@ -23,7 +28,7 @@
           <tbody>
             <tr v-for="collection in myCollections" class="hoverable" :key="collection.id"
             @click="viewCollectionDetail(collection.id)">
-              <td>{{ collection.name }}</td>
+              <td class="table-name">{{ collection.name }}</td>
               <td>{{ collection.game }}</td>
               <td>{{ formatDate(collection.date_updated) }}</td>
             </tr>
@@ -33,15 +38,123 @@
       </div>
     </div>
 
-    <div @click="showSharedCollections = !showSharedCollections" class="topic">Shared Collections
-      <div class="is-pulled-right mr-2">
+    <div @click="showSharedCollections = !showSharedCollections" class="topic">
+      Shared Collections <span v-if="!$store.state.user.is_subscriber"> (Subscribers only)</span>
+      <div v-if="$store.state.user.is_subscriber" class="is-pulled-right mr-2">
         <fa v-if="showSharedCollections" icon="angle-down"/>
         <fa v-if="!showSharedCollections" icon="angle-right"/>
       </div>
     </div>
 
-    <div v-if="showSharedCollections">
-      collection shared
+    <div v-if="showSharedCollections & $store.state.user.is_subscriber">
+
+      <div class="search-form">
+        <label class="label m-2">Only officials</label>
+        <input type="checkbox" class="checkbox" v-model="only_officials">
+        <label for="" class="label m-2 ml-5">Language</label>
+        <select v-model="language" class="m-2 is-small">
+          <option value="all">--All--</option>
+          <option value="ar">Arabic</option>
+          <option value="cs">Czech</option>
+          <option value="zh">Chinese</option>
+          <option value="da">Danish</option>
+          <option value="nl">Dutch</option>
+          <option value="en">English</option>
+          <option value="fi">Finnish</option>
+          <option value="fr">French</option>
+          <option value="de">German</option>
+          <option value="el">Greek</option>
+          <option value="hi">Hindi</option>
+          <option value="hu">Hungarian</option>
+          <option value="it">Italian</option>
+          <option value="ja">Japanese</option>
+          <option value="ko">Korean</option>
+          <option value="no">Norwegian</option>
+          <option value="fa">Persian</option>
+          <option value="pl">Polish</option>
+          <option value="pt">Portuguese</option>
+          <option value="ru">Russian</option>
+          <option value="es">Spanish</option>
+          <option value="sv">Swedish</option>
+          <option value="th">Thai</option>
+          <option value="tr">Turkish</option>
+          <option value="vi">Vietnamese</option>
+
+        </select>
+      </div>
+      <div class="search-form">
+        <span class="label m-1">by</span>
+        <div class="select m-1 is-small">
+          <select v-model="searchBy">
+            <option value="game">Game</option>
+            <option value="name">Collection Name</option>
+            <option value="author">Author</option>
+          </select>
+        </div>
+        <input class="input m-1 is-small" type="text" placeholder="Your search..." v-model="searchText">
+        <button class="button is-primary m-1 is-small" @click="searchSharedCollections">Go</button>
+      </div>
+      <div v-if="sharedCollections.length">
+        <table class="table is-fullwidth">
+          <thead>
+            <tr>
+              <th>name</th>
+              <th>game</th>
+              <th>actions</th>
+              <th>author</th>
+              <th>language</th>
+              <th>last updated</th>
+            </tr>
+          </thead>
+          <tbody>
+            <tr v-for="collection in sharedCollections" :key="collection.id">
+              <td class="table-name">
+                  {{ collection.name }}
+              </td>
+              <td>
+                <OfficialMark v-if="collection.is_official" :collection="collection"/>
+                {{ collection.game }}
+              </td>
+              <td>
+                <div class="dropdown is-hoverable">
+                  <div class="dropdown-trigger">
+                    <button class="button is-small" aria-haspopup="true" aria-controls="dropdown-menu3">
+                      <span>actions</span>
+                      <span class="icon is-small">
+                        <i class="fas fa-angle-down" aria-hidden="true"></i>
+                        <fa icon="angle-down"/>
+                      </span>
+                    </button>
+                  </div>
+                  <div class="dropdown-menu" id="dropdown-menu3" role="menu">
+                    <div class="dropdown-content">
+                      <a href="" class="dropdown-item">
+                        Use as new campain
+                      </a>
+                      <a href="" class="dropdown-item">
+                        Exchange Interface
+                      </a>
+                      <a href="" class="dropdown-item">
+                        <span>
+                          <fa icon="heart" style="color: red;"/>
+                          add to favorites
+                        </span>
+
+                      </a>
+                    </div>
+                  </div>
+
+                </div>
+              </td>
+              <td>{{ collection.author }}</td>
+              <td>{{ collection.language }}</td>
+              <td>{{ formatDate(collection.date_updated) }}</td>
+            </tr>
+          </tbody>
+
+        </table>
+      </div>
+
     </div>
 
   </div>
@@ -49,20 +162,52 @@
 
 <script>
 import axios from 'axios'
+import OfficialMark from './components/OfficialMark.vue'
 
 export default {
   name: 'CollectionsView',
+  components: {
+    OfficialMark,
+  },
   data(){
     return {
+      searchBy: 'game',
+      searchText: '',
+      language: 'en',
+      only_officials: false,
       showMyCollections: true,
       showSharedCollections: false,
       myCollections: [],
+      sharedCollections: [],
     }
   },
   beforeMount(){
     this.getMyCollectionList()
   },
   methods: {
+    searchSharedCollections(){
+      this.sharedCollections = []
+      axios({
+        method: 'get',
+        url: "campains/get_shared_collections/",
+        headers: {
+          'Authorization': `Token ${this.$store.state.token}`
+        },
+        params: {
+          'search_by': this.searchBy,
+          'search_text': this.searchText,
+          'language': this.language,
+          'only_officials': this.only_officials,
+        }
+      })
+      .then(response => {
+        this.sharedCollections = response.data
+        console.log("Response: ", response.data)
+      })
+      .catch(error => {
+        console.log(error);
+      });
+    },
     viewCollectionDetail(id){
       this.$router.push({name: 'collectionDetailView', params: {id: id}})
     },
@@ -97,7 +242,7 @@ export default {
       })
       .then(response => {
         console.log(response.data);
-        this.myCollections.push(response.data)
+        this.myCollections.unshift(response.data)
       })
       .catch(error => {
         console.log(error);
@@ -126,5 +271,15 @@ export default {
   background-color: rgb(176, 255, 210);
   color: rgb(92, 92, 92);
   cursor: pointer;
+}
+.search-form {
+  display: flex;
+  margin-bottom: 5px;
+  margin-top: 5px;
+}
+
+.table-name{
+  font-weight: bold;
+  color: gray;
 }
 </style>
---
+++ src/views/campains/collections/components/OfficialMark.vue
@@ -0,0 +1,60 @@
+<template>
+  <span class="official" :class="{'hoverable-official': has_url}">
+    <a v-if="has_url" :href="collection.official_url" target="_blank" style="text-decoration: none;">
+      official
+    </a>
+    <span v-else>
+    official
+    </span>
+  </span>
+
+
+</template>
+
+<script>
+export default {
+  name: "OfficialMark",
+  props: [
+    'collection',
+  ],
+  computed: {
+    has_url() {
+      if (this.collection.official_url === null) {
+        return false
+      } else {
+        return true
+      }
+    },
+  },
+}
+</script>
+
+<style scoped>
+
+a {
+  color: black;
+  text-decoration: none;
+}
+
+a:visited {
+  color: black;
+}
+
+.hoverable-official:hover {
+  background-color: gold;
+  color: black;
+  cursor: pointer;
+}
+
+.official {
+  color: black;
+  background-color: goldenrod;
+  font-weight: bold;
+  font-size: 0.9rem;
+  border: 2px solid black;
+  border-radius: 5px;
+  margin: 4px;
+  user-select: none;
+}
+
+</style>
